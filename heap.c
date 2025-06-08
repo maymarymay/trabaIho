e59f32b2a2bb5ad7fa4af9708a1aa627745f6cd9
mary <scmaryanasc@gmail.com>
2025-06-07 22:30:29 -0400

    mudei umas funçoes

--- heap.c
+++ heap.c
@@ -1,7 +1,5 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<assert.h>
+#include <stdio.h> 
+#include <stdlib.h> 
 #include "heap.h" 
 
 /* FUNCOES HEAP */
@@ -40,63 +38,43 @@ theap *contstroi_heap (int i){
 void desce(theap * heap,int i){
     int e = filho_esq(i);
     int d = filho_dir(i);
-    int imin = i;
-    if (e < heap->tam && heap->lista[e].distancia > heap->lista[imin].distancia)
-        imin = e;
-    if (d < heap->tam && heap->lista[d].distancia > heap->lista[imin].distancia)
-        imin = d;
-    if (imin != i){
-        troca(&(heap->lista[i]),&(heap->lista[imin]));
-        desce(heap,imin);
+    int maior = i;
+    if (e < heap->tam && heap->lista[e].distancia > heap->lista[maior].distancia) {
+        maior = esq;
+    }
+    if (d < heap->tam && heap->lista[d].distancia > heap->lista[maior].distancia) {
+        maior = d;
+    }
+
+    if (maior != i) {
+        troca(&heap->lista[i], &heap->lista[maior]);
+        desce(heap, maior);
     }
 }
 
-void sobe(theap * heap,int i){
+void sobe(theap *heap, int i) {
     int p;
-    if ((i > 0) && (i < heap->tam)){
+    while (i > 0) {
         p = pai(i);
-        while (heap->lista[i].distancia < heap->lista[p].distancia){
-            troca(&heap->lista[i],&heap->lista[p]);
+        if (heap->lista[i].distancia < heap->lista[p].distancia) {
+            troca(&heap->lista[i], &heap->lista[p]);
             i = p; 
-            p = pai(i);   
         }
     }
 }
 
-
-int heap_insere(theap * heap, nodeHeap n){
-   if (heap->tam < heap->cap){
+void insere(theap *heap, nodeHeap n) {
+    if (heap->tam < heap->cap) {
         heap->lista[heap->tam] = n;
+        sobe(heap, heap->tam);
         heap->tam++;
-        sobe(heap,heap->tam-1);
-        return EXIT_SUCCESS;
-    }else{
-        return EXIT_FAILURE;
+    } else if (n.distancia < heap->lista[0].distancia) {
+        heap->lista[0] = n;
+        desce(heap, 0);
     }
 }
 
-/////////nsei se daria certo mas vou deixar aqui por preucas..... 
-//void insere (theap*heap, nodeHeap n)
-//{
-//   if (heap->tam == heap->cap)
-//   {
-//        if (n.distancia<heap->lista[heap->tam-1].distancia)
-//        {
-//            heap->lista[heap->tam-1] = n;
-//            troca(&heap->lista[0], &heap->lista[heap->tam-1]);
-//            desce(heap, 0);
-//        }
-//    }
-//    else
-//    {
-//        heap->lista[heap->tam] = n;
-//        heap->tam++;
-//        int i = heap->tam-1;
-//        sobe(heap,i);
-//    }
-//}
-
-void libera(theap*heap)
+void liberar(theap *heap) 
 {
     if (heap == NULL)
     {
